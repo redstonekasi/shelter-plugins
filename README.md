f3f008f1e3a7ee358636fea1a1e66b025c106b8e
kasimir <kasimir@k6.tf>
2026-06-10 23:44:50 +0200

    add mention controls

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
 # kasimir's shelter plugins
 
-To install these, copy their URLs into [shelter](https://github.com/uwu/shelter).
-
-## No Call Idle
-```
-https://redstonekasi.github.io/shelter-plugins/no-call-idle/
-```
-Prevents Discord from disconnecting you after 5 minutes of being in an empty call.
-
-<!--
-## PEN
-```
-Soon™
-```
-Allows you to use UserCSS inside of Discord.
--->
+Plugins for [shelte¶](https://shelter.uwu.network), see the [generated plugin index](https://redstonekasi.github.io/shelter-plugins).
---
+++ plugins/mention-controls/index.js
@@ -0,0 +1,27 @@
+const {
+	flux: {
+		storesFlat: {
+			UserStore
+		},
+		intercept,
+	},
+} = shelter;
+
+let currentId = UserStore.getCurrentUser().id;
+const currentMention = () =>  `<@${currentId}>`;
+
+// Might as well just do this instead of subscribing
+export const onUnload = intercept((data) => {
+	if (data.type === "CONNECTION_OPEN") {
+		currentId = data.user.id;
+	}
+
+	if (data.type !== "MESSAGE_CREATE" && data.type !== "MESSAGE_UPDATE") return;
+	const { message } = data;
+
+	if (message.referenced_message?.author?.id !== currentId) return;
+	if (!message.mentions.some((m) => m.id === currentId)) return;
+	if (message.content.includes(currentMention())) return;
+
+	message.mentions = message.mentions.filter((m) => m.id !== currentId);
+});
